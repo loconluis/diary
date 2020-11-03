22eebe5b682b6e73332788b995f870399419a253
Luis Locon <luis.locon@gmail.com>
2020-11-02 23:14:02 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import 'react-native-gesture-handler';
+import { name as Diary } from './app.json';
+
+AppRegistry.registerComponent(Diary, () => App);
